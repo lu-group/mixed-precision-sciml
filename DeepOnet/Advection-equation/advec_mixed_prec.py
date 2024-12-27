6e377e92f5f38403d1d9405ea210f14cbc62a281
Joel Hayford <117128338+Jhayford@users.noreply.github.com>
2024-12-27 13:13:48 -0500

    Update advec_mixed_prec.py

--- DeepOnet/Advection-equation/advec_mixed_prec.py
+++ DeepOnet/Advection-equation/advec_mixed_prec.py
@@ -17,7 +17,7 @@ import deepxde as dde
 from deepxde.backend import tf
 
 dde.config.enable_xla_jit(mode = False)
-dde.config.set_default_float('float32')
+dde.config.set_default_float('float16')
 
 policy = mixed_precision.Policy('mixed_float16')
 mixed_precision.set_global_policy(policy)
@@ -28,9 +28,9 @@ def get_data(filename):
     nx = 40
     nt = 40
     data = np.load(filename)
-    x = data["x"].astype(np.float32)
-    t = data["t"].astype(np.float32)
-    u = data["u"].astype(np.float32)  # N x nt x nx
+    x = data["x"].astype(np.float16)
+    t = data["t"].astype(np.float16)
+    u = data["u"].astype(np.float16)  # N x nt x nx
 
     u0 = u[:, 0, :]  # N x nx
     xt = np.vstack((np.ravel(x), np.ravel(t))).T
@@ -50,16 +50,18 @@ def main():
     )
 
     model = dde.Model(data, net)
+    adam = tf.keras.optimizers.Adam(epsilon=1e-04)
     model.compile(
         "adam",
         lr=1e-3,
         decay=("inverse time", 1, 1e-4),
         metrics=["mean l2 relative error"],
+        loss = "mean l2 relative error",
     )
     # IC1
     # losshistory, train_state = model.train(epochs=100000, batch_size=None)
     # IC2
-    losshistory, train_state = model.train(epochs=2500, batch_size=None)
+    losshistory, train_state = model.train(epochs=250000, batch_size=None)
 
     y_pred = model.predict(data.test_x)
     np.savetxt("y_pred_deeponet.dat", y_pred[0].reshape(nt, nx))
@@ -69,3 +71,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+memory_used = tf.config.experimental.get_memory_info('GPU:0')['peak']
+print("memory used:", memory_used)
